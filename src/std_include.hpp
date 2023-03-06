f520b407ad87145a8df2ee932fe40f6a9d344dd5
Edo <edoardo.sanguineti222@gmail.com>
2023-03-06 15:09:44 +0000

    maint: delete pragma statements

--- src/std_include.hpp
+++ src/std_include.hpp
@@ -1,33 +1,6 @@
 #ifdef _WIN32
 #pragma once
 
-#pragma warning(push)
-#pragma warning(disable: 4018)
-#pragma warning(disable: 4100)
-#pragma warning(disable: 4127)
-#pragma warning(disable: 4244)
-#pragma warning(disable: 4297)
-#pragma warning(disable: 4458)
-#pragma warning(disable: 4702)
-#pragma warning(disable: 4804)
-#pragma warning(disable: 4806)
-#pragma warning(disable: 4996)
-#pragma warning(disable: 5054)
-#pragma warning(disable: 6011)
-#pragma warning(disable: 6297)
-#pragma warning(disable: 6385)
-#pragma warning(disable: 6386)
-#pragma warning(disable: 6387)
-#pragma warning(disable: 26110)
-#pragma warning(disable: 26451)
-#pragma warning(disable: 26444)
-#pragma warning(disable: 26451)
-#pragma warning(disable: 26489)
-#pragma warning(disable: 26495)
-#pragma warning(disable: 26498)
-#pragma warning(disable: 26812)
-#pragma warning(disable: 28020)
-
 #define WIN32_LEAN_AND_MEAN
 
 #include <Windows.h>
@@ -76,9 +49,6 @@
 #include <gsl/gsl>
 
 #ifdef _WIN32
-#pragma warning(pop)
-#pragma warning(disable: 4100)
-
 #pragma comment(lib, "ntdll.lib")
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "urlmon.lib" )
